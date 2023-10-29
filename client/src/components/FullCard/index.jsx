16703fb31cbf9a616525ab1301c00484a169aedb
push-ebx <nikita.keyn@yandex.ru>
2023-10-29 22:25:34 +0300

    fix FullCard, add effects and add vercel.json

--- client/src/components/FullCard/index.jsx
+++ client/src/components/FullCard/index.jsx
@@ -9,20 +9,11 @@ const FullCard = ({phoneNumber,
   return (
     <div className={styles.fullCard}>
       <div className={styles.rows}>
-        <div className={styles.rowNames}>
-          <span>Телефон:</span>
-          <span>Почта:</span>
-          <span>Дата приема:</span>
-          <span>Должность:</span>
-          <span>Подразделение:</span>
-        </div>
-        <div className={styles.rowValues}>
-          <span>{phoneNumber}</span>
-          <span>{email}</span>
-          <span>{date}</span>
-          <span>{position}</span>
-          <span>{division}</span>
-        </div>
+        <span>Телефон:</span> <span>{phoneNumber}</span>
+        <span>Почта:</span> <span>{email}</span>
+        <span>Дата приема:</span> <span>{date}</span>
+        <span>Должность:</span> <span>{position}</span>
+        <span>Подразделение:</span> <span>{division}</span>
       </div>
       <div className={styles.extraInfo}>
         <span>Дополнительная информация:</span>
